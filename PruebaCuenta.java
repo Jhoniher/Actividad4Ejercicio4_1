382278ac773bdae3bef460e2da0e1925de0d05f0
Jhony Alpala <jhoniher95@gmail.com>
2023-10-13 14:07:14 -0500

    Agregadas entradas y clases a crear

--- PruebaCuenta.java
+++ PruebaCuenta.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package actividad4_1_herencia;
 
-/**
- *
- * @author Jhony
- */
+package actividad4_1_herencia;
+import java.util.Scanner;
 public class PruebaCuenta {
+    public static void main(String[] args){
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Cuenta de ahorros");
+        System.out.println("Ingrese saldo inicial=$");
+        float saldoInicialAhorros=entrada.nextFloat();
+        System.out.println("Ingrese tasa de interés=");
+        float tasaAhorros=entrada.nextFloat();
+        CuentaAhorros cuenta1=new CuentaAhorros(saldoInicialAhorros, tasaAhorros);
+        System.out.println("Ingresar cantidad a consignar: $");
+        float cantidadDepositar=entrada.nextFloat();
+        cuenta1.consignar(cantidadDepositar);
+        System.out.println("Ingresar cantidad a retirar: $");
+        float cantidadRetirar=entrada.nextFloat();
+        cuenta1.retirar(cantidadRetirar);
+        cuenta1.extractoMensual();
+        cuenta1.imprimir();
+        
+    }
     
 }
